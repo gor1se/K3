3d5599e31eae6ebba0ae46397c49f19a0d1d15c5
gor1se <andreas.schmidt.lth@t-online.de>
2021-10-18 20:43:13 +0200

    Einfache Subtraktionsaufgaben hinzugefügt (LvL 1-2)

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const aufgaben = require(__dirname + "/aufgaben.js");
 
 let ary = [];
 for (let i = 1; i < 11; i++) {
-    ary.push(aufgaben.lvl_1_add_v1(i));
+    ary.push(aufgaben.lvl_2_sub_v1(i));
 }
 console.log(ary);
--- aufgaben.js
+++ aufgaben.js
@@ -37,6 +37,45 @@ exports.lvl_2_add_v1 = function(id) {
     return obj;
 };
 
+exports.lvl_1_sub_v1 = function(id) {
+    let obj = {
+        id: id,
+        aufgabe: "",
+        antwort: 0,
+        antwort_f1: 0,
+        antwort_f2: 0,
+        level: 1,
+        art: "Multiple Choice"
+    };
+    let ran_1 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    let ran_2 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    obj.aufgabe = ran_1 + " - " + ran_2;
+    obj.antwort = ran_1 - ran_2;
+    obj.antwort_f1 = obj.antwort + Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    obj.antwort_f2 = obj.antwort - Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    return obj;
+};
+
+exports.lvl_2_sub_v1 = function(id) {
+    let obj = {
+        id: id,
+        aufgabe: "",
+        antwort: 0,
+        antwort_f1: 0,
+        antwort_f2: 0,
+        level: 2,
+        art: "Multiple Choice"
+    };
+    let ran_1 = Math.floor(Math.random() * (30 - 20 + 1)) + 20;
+    let ran_2 = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+    let ran_3 = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+    obj.aufgabe = ran_1 + " - " + ran_2 + " - " + ran_3;
+    obj.antwort = ran_1 - ran_2 - ran_3;
+    obj.antwort_f1 = obj.antwort + Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    obj.antwort_f2 = obj.antwort - Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    return obj;
+};
+
 // Ein Aufgabenobject sollte folgende Eigenschaften haben:
 // - ID
 // - Aufgabe
